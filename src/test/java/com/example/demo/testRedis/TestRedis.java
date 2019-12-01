0b3dae71e56d0f064dd9764260fc4a3261aae3da
mrh <smilehecb@163.com>
2019-12-01 17:38:33 +0800

    testaddfile

---
+++ src/test/java/com/example/demo/testRedis/TestRedis.java
@@ -0,0 +1,13 @@
+package com.example.demo.testRedis;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRedis {
+
+    @Test
+    public void testRe(){
+        System.out.print("nihao");
+    }
+
+
+}
